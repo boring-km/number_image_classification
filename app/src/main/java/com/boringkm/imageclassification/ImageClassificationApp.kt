d8e902df62f4195f8014cf92df5c49a751b679e6
boring-km <kms0644804@naver.com>
2022-08-19 06:13:41 +0900

    gradle 의존성 버전 최신화 및 Hilt 기본 세팅, coroutines 모듈 추가

---
+++ app/src/main/java/com/boringkm/imageclassification/ImageClassificationApp.kt
@@ -0,0 +1,9 @@
+package com.boringkm.imageclassification
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ImageClassificationApp: Application() {
+
+}
